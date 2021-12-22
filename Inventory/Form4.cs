8535b53d7de994147cd3a54d9e561131c5ac5f17
KomissarovArtem <693020370570080gfh@gmail.com>
2021-12-22 18:11:00 +0400

    Добавил заготовку окна Отчёт

---
+++ Inventory/Form4.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Inventory
+{
+    public partial class Form4 : Form
+    {
+        public Form4()
+        {
+            InitializeComponent();
+        }
+
+        private void Form4_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+            this.Hide();
+            MainForm form1 = new MainForm();
+            form1.Show();
+        }
+    }
+}
